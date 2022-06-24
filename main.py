32ea339fe9663af4f991f00321288217705239f3
haig <haigharutunian@protonmail.com>
2022-06-24 00:09:07 -0400

    done

--- main.py
+++ main.py
@@ -1,16 +1,45 @@
-# This is a sample Python script.
+import time
+import threading
+from pynput.mouse import Button, Controller
+from pynput.keyboard import Listener, KeyCode
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+start_end = KeyCode(char='a')
+exit_key = KeyCode(char='z')
 
+class auto_clicker_class(threading.Thread):
+    def __init__(self):
+        super(auto_clicker_class, self).__init__()
+        self.delay = 0.0000000001
+        self.button = Button.left
+        self.running = False
+        self.program_run = True
+    def begin_clicking(self):
+        self.running = True
+    def clicking_stop(self):
+        self.running = False
+    def exit(self):
+        self.clicking_stop()
+        self.program_run = False
+    def run(self):
+        while self.program_run:
+            while self.running:
+                mouse_ob.click(self.button)
+                time.sleep(self.delay)
+            time.sleep(0.000000000001)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+mouse_ob = Controller()
+t = auto_clicker_class()
+t.start()
 
+def fun(k):
+    if k == start_end:
+        if t.running:
+            t.clicking_stop()
+        else:
+            t.begin_clicking()
+    elif k == exit_key:
+        t.exit()
+        listener.stop()
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+with Listener(on_press=fun) as listener:
+    listener.join()
